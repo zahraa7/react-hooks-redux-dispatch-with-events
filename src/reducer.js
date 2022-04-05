816aa5c060637f9f3ad0262983be0205dacd3402
zahraa7 <zahraa.knass@hotmail.com>
2022-04-05 16:51:27 +0300

    done

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,28 @@
 // add code snippets from README
+let state;
+let button = document.getElementById("button");
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+});
+
+dispatch({ type: "@@init" });
